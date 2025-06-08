bf806fb7f1033cd45180031c1900131866ce8ddb
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-08 21:30:59 +0530

    pattern7 uploaded

---
+++ pattern7.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+// *
+// * * 
+// * * *
+// * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+int main2()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+int main1()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}

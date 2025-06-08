3398051700bd4c85202d6f5f48644023d0e448ee
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-08 17:53:17 +0530

    pattern1 updated

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,15 +1,75 @@
 #include <iostream>
 using namespace std;
 
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+// 5 4 3 2 1 
+// 5 4 3 2 1
 int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 5; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// 5 5 5 5 5
+// 4 4 4 4 4
+// 3 3 3 3 3
+// 2 2 2 2 2
+// 1 1 1 1 1
+int main3()
+{
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+int main2()
 {
     for (int i = 1; i <= 5; i++)
     {
         for (int j = 1; j <= 5; j++)
         {
-            cout << "*"
+            cout << j
                  << " ";
         }
         cout << "\n";
     }
+    return 0;
+}
+
+// 1 1 1 1 1
+// 2 2 2 2 2
+// 3 3 3 3 3
+// 4 4 4 4 4
+// 5 5 5 5 5
+int main1()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
 }

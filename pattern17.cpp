487ef1b3bfd1b1d4044748c5bc1a444e29a57a86
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-09 16:37:40 +0530

    pattern17 updated

--- pattern17.cpp
+++ pattern17.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+// 1               1 
+// 1 2           2 1 
+// 1 2 3       3 2 1 
+// 1 2 3 4   4 3 2 1 
+// 1 2 3 4 5 4 3 2 1
+
 int main()
 {
     int k = 7;

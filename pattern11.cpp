98f46743873368be818d3ffa0e30f49cb279a74c
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-09 13:23:29 +0530

    Kajukatli Pattern Uploaded with spaces

---
+++ pattern11.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+//           *
+//         *   *
+//       *       *
+//     *           *
+//   *               *
+//     *           *
+//       *       *
+//         *   *
+//           *
+
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            if (j == 1 || j == i)
+            {
+                cout << "*  "
+                     << " ";
+            }
+            else
+            {
+                cout << "    "; // 4 Space
+            }
+        }
+        cout << "\n";
+    }
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            if (j == 1 || j == 6 - i)
+            {
+                cout << "*  "
+                     << " ";
+            }
+            else
+            {
+                cout << "    "; // 4 Space
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}

222e98d50c035d21b00065fda6c972c75f5d41de
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-09 13:00:44 +0530

    Diamond Pattern Uploaded

---
+++ diamond_pattern.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+//           *
+//         *   *
+//       *   *   *
+//     *   *   *   *
+//   *   *   *   *   *
+//     *   *   *   *
+//       *   *   *
+//         *   *
+//           *
+
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*  "
+                 << " ";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << "*  "
+                 << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}

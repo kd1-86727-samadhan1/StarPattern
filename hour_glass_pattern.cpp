aa72e812fddb7eaab3153d058e4f41ef6267c414
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-09 15:09:31 +0530

    Hour Glass Pattern Uploaded

---
+++ hour_glass_pattern.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << "*  "
+                 << " "; // * + 2 Space
+        }
+        cout << "\n";
+    }
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*  "
+                 << " "; // * + 2 Space
+        }
+        cout << "\n";
+    }
+    return 0;
+}

c21e7ded0f7d9d6392b2007bb97d4f3628adfbc9
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-09 17:38:54 +0530

    pattern19 updated

--- pattern19.cpp
+++ pattern19.cpp
@@ -1,61 +1,33 @@
 #include <iostream>
 using namespace std;
 
-// *                 *
-// * *             * *
-// * * *         * * *
-// * * * *     * * * *
-// * * * * * * * * * *
-// * * * *     * * * *
-// * * *         * * *
-// * *             * *
-// *                 *
+//           *   
+//         *   *
+//       *       *
+//     *           *
+//   *   *   *   *   *
 
 int main()
 {
-    for (int i = 1; i <= 4; i++)
+    for (int i = 1; i <= 5; i++)
     {
-        // Left wing
-        for (int j = 1; j <= i; j++)
+
+        for (int j = 1; j <= 6 - i; j++)
         {
-            cout << "*"
+            cout << " "
                  << " ";
         }
-
-        // Spaces between wings
-        for (int j = 1; j <= 2 * (5 - i); j++)
-        {
-            cout << "  "; // 2 Space
-        }
-
-        // Right wing
         for (int j = 1; j <= i; j++)
         {
-            cout << "*"
-                 << " ";
-        }
-        cout << "\n";
-    }
-
-    for (int i = 5; i >= 1; i--)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*"
-                 << " ";
-        }
-
-        // Spaces between wings
-        for (int j = 1; j <= 2 * (5 - i); j++)
-        {
-            cout << "  "; // 2 Space
-        }
-
-        // Right wing
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*"
-                 << " ";
+            if (j == 1 || i == 5 || j == i)
+            {
+                cout << "*  "
+                     << " ";
+            }
+            else
+            {
+                cout << "    "; // 4 Space
+            }
         }
         cout << "\n";
     }

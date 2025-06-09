8c59b53ffe60528940e9d35d6608498ae4743af4
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-09 15:30:26 +0530

    pattern13 uploaded

--- pattern13.cpp
+++ pattern13.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//  *               *   
+//     *           *
+//       *       *
+//         *   *
+//           *   
+//         *   *
+//       *       *
+//     *           *
+//   *               *
+
 int main()
 {
     for (int i = 1; i <= 4; i++)

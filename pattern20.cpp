b700b0522e26d9b43cbfb1fc105424131e072a1d
Samadhan1661 <aiwalesamadhan16@gmail.com>
2025-06-10 06:18:12 +0530

    pattern20 uploaded

---
+++ pattern20.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//   *   *   *   *   *
+//     *           *
+//       *       *
+//         *   *
+//           *
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " "
+                 << " ";
+        }
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            if (j == 1 || i == 1 || j == 6 - i)
+            {
+                cout << "*  "
+                     << " ";
+            }
+            else
+            {
+                cout << "    "; // 4 Space
+            }
+        }
+
+        cout << "\n";
+    }
+    return 0;
+}
